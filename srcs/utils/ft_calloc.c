924eac1dbeca71de20974872f1b63edf2e85293e
gsilva-v <gaa.silva53@gmail.com>
2022-02-02 13:48:53 -0300

    removed comment of calloc

--- srcs/utils/ft_calloc.c
+++ srcs/utils/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: gsilva-v <gsilva-v@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 19:43:55 by flavio            #+#    #+#             */
-/*   Updated: 2022/02/02 09:31:42 by gsilva-v         ###   ########.fr       */
+/*   Updated: 2022/02/02 13:46:38 by gsilva-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,6 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*r;
 
-	// if (nmemb * size > INT_MAX)
-	// {
-	// 	errno = 12;
-	// 	return (0);
-	// }
 	r = malloc(nmemb * size);
 	if (r)
 	{
