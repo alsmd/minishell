61a045fb5f31513e7e538a432ec97f068e14b97d
gsilva-v <gaa.silva53@gmail.com>
2022-03-09 17:59:20 -0300

    fix echo

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: gsilva-v <gsilva-v@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 12:44:57 by flda-sil          #+#    #+#             */
-/*   Updated: 2022/03/09 09:08:46 by gsilva-v         ###   ########.fr       */
+/*   Updated: 2022/03/09 17:57:48 by gsilva-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	my_echo(char **argv)
 	{
 		broke_line = 1;
 		i = 2;
-		while (!ft_strncmp(argv[i], "-n", -1))
+		while (argv[i] && !ft_strncmp(argv[i], "-n", -1))
 			i++;
 	}
 	while (argv[i])
